386bfcfbe39d8641d5a0cd86dff29dbdb1ee3690
cwanasmoiz <142308572+cwanasmoiz@users.noreply.github.com>
2026-08-19 13:50:57 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    allowedHosts: ["nodejs-1651733-6566586.cloudwaysapps.com"],
+      allowedHosts: [".cloudwaysnodeapps.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
